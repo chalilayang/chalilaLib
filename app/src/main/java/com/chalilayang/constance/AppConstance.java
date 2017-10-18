1da52c0ccf4a6ae1e2bb0107dac48cbae34320c0
chalilayang <chalilayang@sohu-inc.com>
2017-10-18 18:59:15 +0800

    Initial commit

---
+++ app/src/main/java/com/chalilayang/constance/AppConstance.java
@@ -0,0 +1,9 @@
+package com.chalilayang.constance;
+
+/**
+ * Created by chalilayang on 2017/10/18.
+ */
+
+public class AppConstance {
+    public static final boolean DEBUG = true;
+}
---
+++ app/src/main/java/com/chalilayang/constance/UrlConstance.java
@@ -0,0 +1,9 @@
+package com.chalilayang.constance;
+
+/**
+ * Created by chalilayang on 2017/10/18.
+ */
+
+public class UrlConstance {
+    public static final String BASE_URL = "";
+}
