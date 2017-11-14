6e4de3b4169725f328996fcc2b3674ad2ec61620
chalilayang <chalilayang@gmail.com>
2017-11-14 23:17:45 +0800

    logo海报控件

---
+++ app/src/main/java/com/reshape/app/customview/LogoImageView.java
@@ -0,0 +1,81 @@
+package com.reshape.app.customview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+import com.chalilayang.scaleview.ScaleCalculator;
+import com.reshape.app.R;
+
+/**
+ * Created by chalilayang on 2017/11/14.
+ */
+
+public class LogoImageView extends AppCompatImageView {
+    private Drawable proLogo;
+    private boolean proLogoVisible;
+
+    private Drawable chnLogo;
+    private boolean chnLogoVisible;
+
+    private int marginForCHNLogo;
+
+    public LogoImageView(Context context) {
+        this(context, null);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LogoImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
+
+    public void setProLogoVisible(boolean flag) {
+        if (flag != proLogoVisible) {
+            proLogoVisible = flag;
+            invalidate();
+        }
+    }
+
+    public void setChnLogoVisible(boolean flag) {
+        if (flag != chnLogoVisible) {
+            chnLogoVisible = flag;
+            invalidate();
+        }
+    }
+
+    private void init(Context context) {
+        marginForCHNLogo = ScaleCalculator.getInstance(context).scaleWidth(30);
+        proLogo = getResources().getDrawable(R.mipmap.pro_logo);
+        chnLogo = getResources().getDrawable(R.mipmap.chinese_logo);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        if (proLogoVisible && width > 0 && height > 0) {
+            int left = width - proLogo.getIntrinsicWidth();
+            int top = 0;
+            int right = width;
+            int bottom = proLogo.getIntrinsicHeight();
+            proLogo.setBounds(left, top, right, bottom);
+            proLogo.draw(canvas);
+        }
+
+        if (chnLogoVisible && width > 0 && height > 0) {
+            int left = width - chnLogo.getIntrinsicWidth() - marginForCHNLogo;
+            int top = 0;
+            int right = width - marginForCHNLogo;
+            int bottom = chnLogo.getIntrinsicHeight();
+            chnLogo.setBounds(left, top, right, bottom);
+            chnLogo.draw(canvas);
+        }
+    }
+}
