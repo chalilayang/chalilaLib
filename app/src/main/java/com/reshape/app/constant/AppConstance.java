8930897c9e0b42595965fcf15055b38407949f1c
chalilayang <chalilayang@gmail.com>
2017-11-17 11:24:33 +0800

    update

--- app/src/main/java/com/reshape/app/constant/AppConstance.java
+++ app/src/main/java/com/reshape/app/constant/AppConstance.java
@@ -9,4 +9,9 @@ public class AppConstance {
 
 
     public static int REQUEST_CODE_LOGIN_ACTIVITY = 1000;
+
+
+    public static final String UMENG_PUSH_API_KEY = "5a0e54ddf29d9817aa0000d7";
+    public static final String UMENG_PUSH_MESSAGE_SECRET = "367df8049e6d8f1cb4a9ef2a2e0cbf38";
+    public static final String UMENG_PUSH_MASTER_SECRET = "8djpfliumffpjnmy4bshylvjrkvjd6qr";
 }
