3e1486af8c9b0fef2a803a994ecd0fb0f8af5ec9
chalilayang <chalilayang@gmail.com>
2017-11-22 18:54:54 +0800

    播放 update

--- app/src/main/java/com/baogetv/app/model/videodetail/customview/PlayerSeekBar.java
+++ app/src/main/java/com/baogetv/app/model/videodetail/customview/PlayerSeekBar.java
@@ -1,6 +1,10 @@
 package com.baogetv.app.model.videodetail.customview;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,6 +15,11 @@ import com.baogetv.app.R;
  */
 
 public class PlayerSeekBar extends View {
+    private Paint seekPaint;
+    private LinearGradient gradient;
+    private int startColor;
+    private int endColor;
+
     public PlayerSeekBar(Context context) {
         this(context, null);
     }
@@ -21,6 +30,25 @@ public class PlayerSeekBar extends View {
 
     public PlayerSeekBar(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        setBackgroundResource(R.drawable.play_seekbar_bg);
+        seekPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        startColor = getResources().getColor(R.color.play_seek_bar_start);
+        endColor =  getResources().getColor(R.color.play_seek_bar_end);
+    }
+
+    /**
+     * Implement this to do your drawing.
+     *
+     * @param canvas the canvas on which the background will be drawn
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (gradient == null) {
+            gradient = new LinearGradient(
+                    0, 0, canvas.getWidth(), 0,
+                    startColor, endColor, Shader.TileMode.CLAMP);
+            seekPaint.setShader(gradient);
+        }
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), seekPaint);
     }
 }
