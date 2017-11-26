2eede1538d685ba7c091629c7fb034f8ee2e5f1b
chalilayang <chalilayang@gmail.com>
2017-11-27 00:09:06 +0800

    用户接口 update

--- app/src/main/java/com/baogetv/app/BaseActivity.java
+++ app/src/main/java/com/baogetv/app/BaseActivity.java
@@ -1,8 +1,13 @@
 package com.baogetv.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.view.ViewCompat;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 
 import com.umeng.message.PushAgent;
@@ -14,6 +19,8 @@ public class BaseActivity extends Activity {
         super.onCreate(savedInstanceState);
         PushAgent.getInstance(this).onAppStart();
 //        initState();
+        setStatusBarColor(getResources().getColor(R.color.black));
+//        translucentStatusBar(this, true);
     }
     protected void initState() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -21,6 +28,55 @@ public class BaseActivity extends Activity {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             //透明导航栏
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    static void translucentStatusBar(Activity activity, boolean hideStatusBarBackground) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            //添加Flag把状态栏设为可绘制模式
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (hideStatusBarBackground) {
+                //如果为全透明模式，取消设置Window半透明的Flag
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                //设置状态栏为透明
+                window.setStatusBarColor(Color.TRANSPARENT);
+                //设置window的状态栏不可见
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            } else {
+                //如果为半透明模式，添加设置Window半透明的Flag
+                window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                //设置系统状态栏处于可见状态
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            }
+            //view不根据系统窗口来调整自己的布局
+            ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                ViewCompat.setFitsSystemWindows(mChildView, false);
+                ViewCompat.requestApplyInsets(mChildView);
+            }
+        }
+    }
+
+    void setStatusBarColor(int statusColor) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            //取消状态栏透明
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //添加Flag把状态栏设为可绘制模式
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //设置状态栏颜色
+            window.setStatusBarColor(statusColor);
+            //设置系统状态栏处于可见状态
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            //让view不根据系统窗口来调整自己的布局
+            ViewGroup mContentView = (ViewGroup) window.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                ViewCompat.setFitsSystemWindows(mChildView, false);
+                ViewCompat.requestApplyInsets(mChildView);
+            }
         }
     }
 
