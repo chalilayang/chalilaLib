46ae2909eb1f8ead5aec455400c15eeff18f8cc0
chalilayang <chalilayang@gmail.com>
2017-12-02 16:58:11 +0800

    update

--- app/src/main/java/com/baogetv/app/model/usercenter/adapter/MyCacheAdapter.java
+++ app/src/main/java/com/baogetv/app/model/usercenter/adapter/MyCacheAdapter.java
@@ -1,253 +1,133 @@
 package com.baogetv.app.model.usercenter.adapter;
 
 import android.content.Context;
-import android.os.Environment;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.baogetv.app.BaseItemAdapter;
+import com.baogetv.app.ItemViewHolder;
 import com.baogetv.app.R;
 import com.baogetv.app.db.DBController;
-import com.baogetv.app.db.domain.MyBusinessInfLocal;
 import com.baogetv.app.db.domain.MyBusinessInfo;
 import com.baogetv.app.downloader.DownloadService;
 import com.baogetv.app.downloader.callback.DownloadManager;
-import com.baogetv.app.downloader.domain.DownloadInfo;
-import com.baogetv.app.model.usercenter.MyDownloadListener;
-import com.baogetv.app.util.FileUtil;
-import com.baogetv.app.util.StorageManager;
 import com.bumptech.glide.Glide;
+import com.chalilayang.scaleview.ScaleCalculator;
 
-import java.io.File;
 import java.lang.ref.SoftReference;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import static com.baogetv.app.downloader.domain.DownloadInfo.STATUS_COMPLETED;
-import static com.baogetv.app.downloader.domain.DownloadInfo.STATUS_REMOVED;
-import static com.baogetv.app.downloader.domain.DownloadInfo.STATUS_WAIT;
+public class MyCacheAdapter
+        extends BaseItemAdapter<MyBusinessInfo, MyCacheAdapter.ViewHolder>
+        implements ItemViewHolder.ItemDeleteListener<MyBusinessInfo>{
 
-public class MyCacheAdapter extends RecyclerView.Adapter<MyCacheAdapter.ViewHolder> {
-
-    private final Context mContext;
-    private final DownloadManager downloadManager;
+    private int margin_8px;
+    private int margin_15px;
+    private int margin_20px;
+    private int margin_30px;
+    private int margin_160px;
+    private DownloadManager downloadManager;
     private DBController dbController;
-    private List<MyBusinessInfo> data;
+
+    protected SoftReference<ItemViewHolder.ItemDeleteListener<MyBusinessInfo>> mDeleteRef;
+    public void setItemDeleteListener(ItemViewHolder.ItemDeleteListener<MyBusinessInfo> listener) {
+        if (listener != null) {
+            mDeleteRef = new SoftReference<>(listener);
+        }
+    }
 
     public MyCacheAdapter(Context context) {
-        mContext = context;
-        data = new ArrayList<>();
-        downloadManager = DownloadService.getDownloadManager(mContext.getApplicationContext());
+        super(context);
+        margin_8px = ScaleCalculator.getInstance(mContext).scaleWidth(8);
+        margin_15px = ScaleCalculator.getInstance(mContext).scaleWidth(15);
+        margin_20px = ScaleCalculator.getInstance(mContext).scaleWidth(20);
+        margin_30px = ScaleCalculator.getInstance(mContext).scaleWidth(30);
+        margin_160px = ScaleCalculator.getInstance(mContext).scaleWidth(160);
+        downloadManager = DownloadService.getDownloadManager(mContext);
         try {
-            dbController = DBController.getInstance(mContext.getApplicationContext());
+            dbController = DBController.getInstance(mContext);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void updateCacheList(List<MyBusinessInfo> list) {
-        data.clear();
-        if (list != null && list.size() > 0) {
-            data.addAll(list);
+    @Override
+    public ViewHolder createItemViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cache_list_item, parent, false);
+        view.getLayoutParams().height = ScaleCalculator.getInstance(mContext).scaleHeight(200);
+        ViewHolder holder = new ViewHolder(view);
+        holder.setItemDeleteListener(this);
+        return holder;
+    }
+
+    @Override
+    public void onDelete(MyBusinessInfo data, int pos) {
+        if (mDeleteRef != null && mDeleteRef.get() != null) {
+            mDeleteRef.get().onDelete(data, pos);
         }
+    }
+
+    public void deleteItem(int pos) {
+        if (pos >= 0 && pos < getItemCount()) {
+            mValues.remove(pos);
+            notifyItemChanged(pos);
+        }
+    }
+
+    public void deleteAllItem() {
+        mValues.clear();
         notifyDataSetChanged();
     }
 
-    public MyBusinessInfo getData(int pos) {
-        return data.get(pos);
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ViewHolder(LayoutInflater.from(mContext).inflate(
-                R.layout.item_download_info, parent, false));
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.bindData(getData(position), position, mContext);
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-
-        private final ImageView iv_icon;
-        private final TextView tv_size;
-        private final TextView tv_status;
-        private final ProgressBar pb;
-        private final TextView tv_name;
-        private final Button bt_action;
-        private DownloadInfo downloadInfo;
+    public class ViewHolder extends ItemViewHolder<MyBusinessInfo> {
+        public final View mView;
+        public final View contentRoot;
+        public final ImageView mImageView;
+        public final TextView title;
+        public final TextView progressTv;
+        public final TextView stateTv;
+        public final TextView startBtn;
+        public final TextView deleteBtn;
+        protected SoftReference<ItemDeleteListener> mDeleteRef;
+        @Override
+        public void bindData(MyBusinessInfo data, int pos) {
+            title.setText(data.getName());
+            Glide.with(mContext).load(data.getIcon()).crossFade().into(mImageView);
+        }
 
         public ViewHolder(View view) {
             super(view);
-            itemView.setClickable(true);
-            iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
-            tv_size = (TextView) view.findViewById(R.id.tv_size);
-            tv_status = (TextView) view.findViewById(R.id.tv_status);
-            pb = (ProgressBar) view.findViewById(R.id.pb);
-            tv_name = (TextView) view.findViewById(R.id.tv_name);
-            bt_action = (Button) view.findViewById(R.id.bt_action);
+            mView = view;
+            contentRoot = view.findViewById(R.id.item_content_view);
+            contentRoot.setOnClickListener(this);
+            contentRoot.setPadding(margin_30px, 0, 0, 0);
+            mImageView = (ImageView) view.findViewById(R.id.video_item_icon);
+            title = (TextView) view.findViewById(R.id.video_title);
+            progressTv = (TextView) view.findViewById(R.id.download_progress_tv);
+            stateTv = (TextView) view.findViewById(R.id.download_progress_state);
+            startBtn = (TextView) view.findViewById(R.id.download_progress_btn);
+            deleteBtn = (TextView) view.findViewById(R.id.btn_delete);
+            deleteBtn.getLayoutParams().width = margin_160px;
+            deleteBtn.setOnClickListener(this);
         }
 
-        @SuppressWarnings("unchecked")
-        public void bindData(final MyBusinessInfo data, int position, final Context context) {
-            Glide.with(context).load(data.getIcon()).into(iv_icon);
-            tv_name.setText(data.getName());
-            downloadInfo = downloadManager.getDownloadById(data.getUrl().hashCode());
-            if (downloadInfo != null) {
-                downloadInfo.setDownloadListener(
-                        new MyDownloadListener(new SoftReference(ViewHolder.this)) {
-                            @Override
-                            public void onRefresh() {
-                                if (getUserTag() != null && getUserTag().get() != null) {
-                                    ViewHolder viewHolder = (ViewHolder) getUserTag().get();
-                                    viewHolder.refresh();
-                                }
-                            }
-                        });
+        public void setItemDeleteListener(ItemDeleteListener<MyBusinessInfo> listener) {
+            if (listener != null) {
+                mDeleteRef = new SoftReference<ItemDeleteListener>(listener);
             }
-            refresh();
-            bt_action.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (downloadInfo != null) {
-                        switch (downloadInfo.getStatus()) {
-                            case DownloadInfo.STATUS_NONE:
-                            case DownloadInfo.STATUS_PAUSED:
-                            case DownloadInfo.STATUS_ERROR:
-                                downloadManager.resume(downloadInfo);
-                                break;
-                            case DownloadInfo.STATUS_DOWNLOADING:
-                            case DownloadInfo.STATUS_PREPARE_DOWNLOAD:
-                            case STATUS_WAIT:
-                                downloadManager.pause(downloadInfo);
-                                break;
-                            case DownloadInfo.STATUS_COMPLETED:
-                                downloadManager.remove(downloadInfo);
-                                break;
-                        }
-                    } else {
-                        String filepath =
-                                StorageManager.getCacheVideoPath()
-                                        + File.separator + StorageManager.generateFileName() + ".apk";
-                        downloadInfo = new DownloadInfo.Builder().setUrl(data.getUrl()).setPath(filepath).build();
-                        downloadInfo.setDownloadListener(
-                                new MyDownloadListener(new SoftReference(ViewHolder.this)) {
-                                    @Override
-                                    public void onRefresh() {
-                                        notifyDownloadStatus();
-                                        if (getUserTag() != null && getUserTag().get() != null) {
-                                            ViewHolder viewHolder = (ViewHolder) getUserTag().get();
-                                            viewHolder.refresh();
-                                        }
-                                    }
-                                });
-                        downloadManager.download(downloadInfo);
-                        MyBusinessInfLocal myBusinessInfLocal = new MyBusinessInfLocal(
-                                data.getUrl().hashCode(),
-                                data.getName(),
-                                data.getIcon(),
-                                data.getUrl()
-                        );
-                        try {
-                            dbController.createOrUpdateMyDownloadInfo(myBusinessInfLocal);
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-            });
         }
 
-        private void notifyDownloadStatus() {
-            if (downloadInfo.getStatus() == STATUS_REMOVED) {
-                try {
-                    dbController.deleteMyDownloadInfo(downloadInfo.getUri().hashCode());
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-        private void refresh() {
-            if (downloadInfo == null) {
-                tv_size.setText("");
-                pb.setProgress(0);
-                bt_action.setText("Download");
-                tv_status.setText("not downloadInfo");
-            } else {
-                switch (downloadInfo.getStatus()) {
-                    case DownloadInfo.STATUS_NONE:
-                        bt_action.setText("Download");
-                        tv_status.setText("not downloadInfo");
-                        break;
-                    case DownloadInfo.STATUS_PAUSED:
-                    case DownloadInfo.STATUS_ERROR:
-                        bt_action.setText("Continue");
-                        tv_status.setText("paused");
-                        try {
-                            pb.setProgress((int) (downloadInfo.getProgress() * 100.0 /
-                                    downloadInfo.getSize()));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        tv_size.setText(FileUtil.formatFileSize(downloadInfo.getProgress()) + "/"
-                                + FileUtil
-                                .formatFileSize(downloadInfo.getSize()));
-                        break;
-
-                    case DownloadInfo.STATUS_DOWNLOADING:
-                    case DownloadInfo.STATUS_PREPARE_DOWNLOAD:
-                        bt_action.setText("Pause");
-                        try {
-                            pb.setProgress((int) (downloadInfo.getProgress() * 100.0 /
-                                    downloadInfo.getSize()));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        tv_size.setText(FileUtil.formatFileSize(downloadInfo.getProgress()) + "/"
-                                + FileUtil
-                                .formatFileSize(downloadInfo.getSize()));
-                        tv_status.setText("downloading");
-                        break;
-                    case STATUS_COMPLETED:
-                        bt_action.setText("Delete");
-                        try {
-                            pb.setProgress((int) (downloadInfo.getProgress() * 100.0 /
-                                    downloadInfo.getSize()));
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        tv_size.setText(FileUtil.formatFileSize(downloadInfo.getProgress()) + "/"
-                                + FileUtil
-                                .formatFileSize(downloadInfo.getSize()));
-                        tv_status.setText("success");
-                        break;
-                    case STATUS_REMOVED:
-                        tv_size.setText("");
-                        pb.setProgress(0);
-                        bt_action.setText("Download");
-                        tv_status.setText("not downloadInfo");
-                    case STATUS_WAIT:
-                        tv_size.setText("");
-                        pb.setProgress(0);
-                        bt_action.setText("Pause");
-                        tv_status.setText("Waiting");
-                        break;
-                }
-
+        @Override
+        public void onClick(View view) {
+            super.onClick(view);
+            if (mRef != null && mRef.get() != null && view == contentRoot) {
+                mRef.get().onItemClick(mData, position);
+            } else if (view == deleteBtn && mDeleteRef != null && mDeleteRef.get() != null) {
+                mDeleteRef.get().onDelete(mData, position);
             }
         }
     }
