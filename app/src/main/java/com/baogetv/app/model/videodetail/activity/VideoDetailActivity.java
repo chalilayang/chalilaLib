bfc20b63c61f20feba4a48b3caa7c36e9f5fe19c
chalilayang <chalilayang@gmail.com>
2017-12-18 23:22:26 +0800

    分享内容update

--- app/src/main/java/com/baogetv/app/model/videodetail/activity/VideoDetailActivity.java
+++ app/src/main/java/com/baogetv/app/model/videodetail/activity/VideoDetailActivity.java
@@ -376,7 +376,7 @@ public class VideoDetailActivity extends BaseActivity implements ShareBoardliste
         shareContent = new ShareContent();
         shareContent.mText = "1";
         UMWeb web = new UMWeb(String.format(UrlConstance.SHARE_BASE_URL, videoDetailBean.getId()));
-        web.setTitle(videoDetailBean.getTitle() + " | " + videoDetailBean.getChannel_name());
+        web.setTitle(videoDetailBean.getTitle() + " | " + "豹哥健身 | 每日精彩健身视频");
         web.setThumb(new UMImage(this, videoDetailBean.getPic_url()));
         web.setDescription(videoDetailBean.getIntro());
         shareContent.mMedia = web;
